103198d9d38b1f7b566070d0be6ffcec54437cb2
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2023-04-29 21:43:42 +0700

    Add UI About Screen

--- lib/view/about_screen.dart
+++ lib/view/about_screen.dart
@@ -10,13 +10,59 @@ class AboutScreen extends StatefulWidget {
 class _AboutAppScreenState extends State<AboutScreen> {
   @override
   Widget build(BuildContext context) {
+    // double screenWidth = MediaQuery.of(context).size.width;
+    // double containerWidth = screenWidth * 0.5;
     return Scaffold(
+      backgroundColor: const Color(0xffEFEFEF),
       appBar: AppBar(
         title: const Text('About'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text('About Screen'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 300,
+            width: double.infinity,
+            color: Colors.white,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/logo_smkn_8_kotang.png',
+                  width: 200,
+                  height: 200,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  'SMK Negeri 8 Kota Tangerang',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'Aplikasi versi 1.0 - Perpustakaan',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Text(
+                  'Perpustakaan merupakan sebuah aplikasi yang mengatur pencatatan data buku, data anggota perpustakaan dan transaksi pinjam dan kembali buku oleh siswa, Aplikasi ini untuk Staff Perpustakaan yang mengelola perpustakaan SMK Negeri 8 Kota Tangerang.',
+                  style: TextStyle(fontSize: 16),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
