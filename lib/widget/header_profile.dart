fc893b7026bc70725f76d0501a2558b2d88e3c38
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2023-05-12 16:44:25 +0700

    Image Profile but not finish

--- lib/widget/header_profile.dart
+++ lib/widget/header_profile.dart
@@ -1,3 +1,8 @@
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -19,15 +24,33 @@ class _HeaderProfileState extends State<HeaderProfile> {
     super.initState();
   }
 
+  PlatformFile? pickedFile;
+
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   String? _uid;
   AdminData? adminData;
 
+  void selectFile() async {
+    final result = await FilePicker.platform.pickFiles();
+    if (result == null) return;
+
+    setState(() {
+      pickedFile = result.files.first;
+    });
+  }
+
+  void uploadFile() async {
+    final path = 'files/${pickedFile!.name}';
+    final file = File(pickedFile!.path!);
+
+    final ref = FirebaseStorage.instance.ref().child(path);
+    ref.putFile(file);
+  }
+
   void getData() async {
-    User? user = _auth.currentUser;
+    User? user = AuthProvider().currentUser;
     _uid = user?.uid;
-    // print(user?.displayName);
     final DocumentSnapshot snapshot = await FirebaseFirestore.instance
         .collection('admin_data')
         .doc(_uid)
@@ -47,38 +70,55 @@ class _HeaderProfileState extends State<HeaderProfile> {
     super.dispose();
   }
 
-
   @override
   Widget build(BuildContext context) {
-    final User? user = AuthProvider().currentUser;
-
-    // final FirebaseFirestore firestore = FirebaseFirestore.instance;
-    // CollectionReference users = firestore.collection('admin_data');
-    // DocumentReference userDoc = users.doc(user?.uid);
-    // userDoc.get().then((DocumentSnapshot documentSnapshot) {
-    //   if (documentSnapshot.exists) {
-    //     print('Name: ${documentSnapshot.data()!['name']}');
-    //   } else {
-    //     print('Document does not exist on the database');
-    //   }
-    // });
-
-    // final User? user = AuthProvider().currentUser;
+    String? imagePath;
 
     return Center(
       child: Column(
         children: [
-          const CircleAvatar(
-            backgroundColor: Colors.black,
-            radius: 80,
-            backgroundImage: AssetImage('assets/images/icon_name.png'),
+          pickedFile != null
+              ? Container(
+                  width: 150,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.black),
+                    // image?: Image.file(File(pickedFile!.path!))
+                  ),
+                  child: Image.file(
+                    File(pickedFile!.path!),
+                    fit: BoxFit.cover,
+                  ),
+                )
+              : Container(
+                  width: 150,
+                  height: 150,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.black),
+                      image: const DecorationImage(
+                        image: NetworkImage(
+                            'https://1.bp.blogspot.com/-FpuMqkbguNo/Xn4X3kZcrII/AAAAAAAAKJk/UjxkC2QK3AQ5EUWk0V1pexrKy0T8ND3wgCLcBGAsYHQ/s1600/kisspng-stock-photography-portrait-illustration-business-aura-financial-planning-build-a-profitable-busine-5cdaef32740dd9.0531019815578519544754.jpg'),
+                        fit: BoxFit.cover,
+                      )),
+                ),
+          ElevatedButton(
+            onPressed: () async {
+              selectFile();
+            },
+            child: const Text('Select Image'),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              uploadFile();
+            },
+            child: const Text('Upload Image'),
           ),
           const SizedBox(
             height: 20,
           ),
           Text(
-            // 'Rizky Faisal Rafi',
-            // user?.uid ?? 'User Name',
             adminData?.name ?? 'Name',
             style: TextStyle(fontWeight: bold, fontSize: 20),
           ),
@@ -87,15 +127,10 @@ class _HeaderProfileState extends State<HeaderProfile> {
           ),
           Text(
             adminData?.email ?? 'Email',
-            // user?.email ?? 'User Email',
             style: TextStyle(fontWeight: reguler, fontSize: 17),
           ),
           const SizedBox(
             height: 4,
-          ),
-          Text(
-            'Laki-laki',
-            style: TextStyle(fontWeight: reguler, fontSize: 17),
           ),
         ],
       ),
